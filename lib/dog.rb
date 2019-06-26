7afe0a479a22df5a0fabb7585d9df0fdef193c23
slumfrog <kishan.anil@gmail.com>
2019-06-26 19:14:15 +0100

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog
+    def name=(dog_name)           
+      @this_dogs_name = dog_name #this is the setter
+    end
+   
+    def name
+      @this_dogs_name            #this is the getter
+    end
